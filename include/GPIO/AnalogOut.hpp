c7e94d37c891803d93107476305d3d4c3cf281d1
Tokoroten <chikurin1014@gmail.com>
2023-01-10 17:18:01 +0900

    Fix AnalogOut
        Move variables(: frequancy, resol_bits, value) into private
        Use const for pwm_channel
        Make methods(: set/getFreq(), set/getResol())
        Rename a variable: resolution_bits -> resol_bits
        Use formatter

--- include/GPIO/AnalogOut.hpp
+++ include/GPIO/AnalogOut.hpp
@@ -1,26 +1,35 @@
 #pragma once
-#include<Arduino.h>
+#include <Arduino.h>
 
 namespace gpio {
-    class AnalogOut {
-        public:
-            /*delete unexpected constractors*/
-            AnalogOut() = delete;
-            AnalogOut(const AnalogOut&) = delete;
-            AnalogOut& operator=(const AnalogOut&) = delete;
-            AnalogOut(const AnalogOut&&) = delete;
-            AnalogOut& operator=(const AnalogOut&&) = delete;
+class AnalogOut {
+private:
+    double frequency;
+    uint8_t resol_bits;
+    uint32_t value;
 
-            explicit AnalogOut(uint8_t pin, uint8_t pwm_ch, double freq, uint8_t resolution_bits = 8) noexcept;     //ピン番号、PWMチャネル、周波数、分解能を指定してコンストラクト
+public:
+    /*delete unexpected constractors*/
+    AnalogOut() = delete;
+    AnalogOut(const AnalogOut &) = delete;
+    AnalogOut &operator=(const AnalogOut &) = delete;
+    AnalogOut(const AnalogOut &&) = delete;
+    AnalogOut &operator=(const AnalogOut &&) = delete;
 
-            const uint8_t pin_num;
-            uint8_t pwm_channel;
-            bool frequency;
-            uint8_t resolution_bits;
-            uint32_t value;
+    explicit AnalogOut(
+        uint8_t pin, uint8_t pwm_ch, double freq,
+        uint8_t resolution_bits =
+            8) noexcept; // ピン番号、PWMチャネル、周波数、分解能を指定してコンストラクト
 
-            void init();    //setup()内で呼び出す
-            void write(uint32_t arg);   //argの値を出力
-            uint32_t read();    //最後の出力値を返す
-    };
-}
+    const uint8_t pin_num;
+    const uint8_t pwm_channel;
+
+    void init();                            // setup()内で呼び出す
+    void write(uint32_t arg);               // argの値を出力
+    uint32_t read();                        // 最後の出力値を返す
+    void setFreq(double freq);              // 周波数を設定
+    double getFreq();                       // 周波数を返す
+    void setResol(uint8_t resolution_bits); // 分解能を設定
+    uint8_t getResol(); // 分解能の値(*分解能: 2^(返り値))を返す
+};
+} // namespace gpio
--- src/GPIO/AnalogOut.cpp
+++ src/GPIO/AnalogOut.cpp
@@ -1,15 +1,14 @@
-#include<Arduino.h>
-#include"GPIO/AnalogOut.hpp"
+#include "GPIO/AnalogOut.hpp"
+#include <Arduino.h>
 
-gpio::AnalogOut::AnalogOut(uint8_t pin, uint8_t pwm_ch, double freq, uint8_t) noexcept :
-    pin_num(pin),
-    pwm_channel(pwm_ch),
-    frequency(freq),
-    resolution_bits(resolution_bits) {}
+gpio::AnalogOut::AnalogOut(uint8_t pin, uint8_t pwm_ch, double freq,
+                           uint8_t resolution_bits) noexcept
+    : pin_num(pin), pwm_channel(pwm_ch), frequency(freq),
+      resol_bits(resolution_bits) {}
 
 void gpio::AnalogOut::init() {
     pinMode(pin_num, OUTPUT);
-    ledcSetup(pwm_channel, frequency, resolution_bits);
+    ledcSetup(pwm_channel, frequency, resol_bits);
     ledcAttachPin(pin_num, pwm_channel);
 }
 
@@ -18,6 +17,14 @@ void gpio::AnalogOut::write(uint32_t arg) {
     ledcWrite(pwm_channel, value);
 }
 
-uint32_t gpio::AnalogOut::read() {
-    return value;
+uint32_t gpio::AnalogOut::read() { return value; }
+
+void gpio::AnalogOut::setFreq(double freq) { frequency = freq; }
+
+double gpio::AnalogOut::getFreq() { return frequency; }
+
+void gpio::AnalogOut::setResol(uint8_t resolution_bits) {
+    resol_bits = resolution_bits;
 }
+
+uint8_t gpio::AnalogOut::getResol() { return resol_bits; }
