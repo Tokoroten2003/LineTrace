0335b76e04aeda82ddeffe58fd0c1e51dc7195fe
Tokoroten <chikurin1014@gmail.com>
2023-02-08 14:41:55 +0900

    Fix bag

--- src/GPIO/AnalogOut.cpp
+++ src/GPIO/AnalogOut.cpp
@@ -2,12 +2,12 @@
 
 #include <Arduino.h>
 
-gpio::AnalogOut::AnalogOut(uint8_t pin, uint8_t pwm_ch, double freq, uint8_t) noexcept :
-    pin_num(pin), pwm_channel(pwm_ch), frequency(freq), resolution_bits(resolution_bits) {}
+gpio::AnalogOut::AnalogOut(uint8_t pin, uint8_t pwm_ch, double freq, uint8_t resolution_bits) noexcept :
+    pin_num(pin), pwm_channel(pwm_ch), frequency(freq), resol_bits(resolution_bits) {}
 
 void gpio::AnalogOut::init() {
     pinMode(pin_num, OUTPUT);
-    ledcSetup(pwm_channel, frequency, resolution_bits);
+    ledcSetup(pwm_channel, frequency, resol_bits);
     ledcAttachPin(pin_num, pwm_channel);
 }
 
