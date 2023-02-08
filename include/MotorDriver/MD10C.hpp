b29076c5aa91eba56b5f203f5edc39bffe8c26fa
Tokoroten <chikurin1014@gmail.com>
2023-02-08 14:59:10 +0900

    Make MD10C not inherit MD

--- include/MotorDriver/MD10C.hpp
+++ include/MotorDriver/MD10C.hpp
@@ -1,12 +1,11 @@
 #pragma once
 #include "GPIO/AnalogOut.hpp"
 #include "GPIO/DigitalOut.hpp"
-#include "MotorDriver/MD.hpp"
 
 #include <Arduino.h>
 
 namespace motorDriver {
-class MD10C : motorDriver::MD<int16_t> {
+class MD10C {
 private:
     gpio::AnalogOut pwm;
     gpio::DigitalOut dir;
@@ -16,7 +15,7 @@ protected:
     int16_t power;
 
 public:
-    /*delete unexpected constractors*/
+    /* delete unexpected constractors */
     MD10C()                         = delete;
     MD10C(const MD10C&)             = delete;
     MD10C& operator=(const MD10C&)  = delete;
@@ -26,9 +25,9 @@ public:
     MD10C(uint8_t pwm_pin, uint8_t pwm_ch);
     MD10C(uint8_t pwm_pin, uint8_t pwm_ch, uint8_t dir_pin, bool usage_dir = true);
 
-    void init() override;                    // setup()内で呼び出す
-    void setPower(int16_t pow = 0) override; // powerの値を設定
-    int16_t getPower() override;             // powerの値を返す
-    void run() override;                     // powerの値を出力
+    void init();                    // setup()内で呼び出す
+    void setPower(int16_t pow = 0); // powerの値を設定
+    int16_t getPower();             // powerの値を返す
+    void run();                     // powerの値を出力
 };
 } // namespace motorDriver
