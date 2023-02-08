750ab82e73382cecf9906b2f8664bfa864c38a43
Tokoroten <chikurin1014@gmail.com>
2023-02-08 15:25:33 +0900

    Remove inclusion of MD.hpp from all.hpp

--- include/all.hpp
+++ include/all.hpp
@@ -4,7 +4,6 @@
 #include "GPIO/AnalogOut.hpp"
 #include "GPIO/DigitalIn.hpp"
 #include "GPIO/DigitalOut.hpp"
-#include "MotorDriver/MD.hpp"
 #include "MotorDriver/MD10C.hpp"
 #include "Sensor/PhotoRefSens.hpp"
 #include "Sensor/Sens.hpp"
